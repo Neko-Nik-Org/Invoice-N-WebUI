dd41b9e1ed533284a73ecb59090312e0e8c367a2
Viraj Gavade <vrajgavade17@gmail.com>
2025-06-16 19:13:57 +0530

    `Update README.md to reflect React + Vite setup and available plugins`

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='bg-cyan-500 text-center text-white text-2xl font-bold p-4'>
+      Welcome to N invoice
+    </div>
+    </>
+  )
+}
+
+export default App
