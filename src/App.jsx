d986e0663a26f4ad5ba0945e4ea49b748a0912ed
flying-spagetti <gnan.lopinti@gmail.com>
2025-07-11 11:53:28 +0530

    Adds Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import HomePage from './pages/HomePage'
 import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout';
 import Login from './pages/Login';
+import InvoicePage from './pages/CreateInvoice';
+import Register from './pages/Register';
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -14,6 +16,8 @@ const routes = createBrowserRouter(
       <Route path='/generateinvoice' element={<GenerateInvoice />}/>
       <Route path='/downloadinvoice' element={<DownloadInvoice />}/>
       <Route path='/login' element={<Login />}/>
+      <Route path='/create-invoice' element={<InvoicePage/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
      </Route>
   )
 );
---
+++ src/pages/CreateInvoice.tsx
@@ -0,0 +1,10 @@
+
+export default function InvoicePage(){
+    const UserName= "Gnan";
+    return (
+        <div className="invoice-page">
+            <h1>Welcome {UserName}, to Invoice-N</h1>
+            <p>Get Started with our tailor made templates for our purpose</p>
+        </div>
+    )
+}
